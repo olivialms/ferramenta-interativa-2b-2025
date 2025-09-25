fb913400773916f57d1c22bc1dfa043e33559805
olivialms <c.olivia@escola.pr.gov.br>
2025-09-24 23:24:57 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,12 @@
 
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'o JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a primeira lei de Newton?',
+    'Lei da inércia'
+)

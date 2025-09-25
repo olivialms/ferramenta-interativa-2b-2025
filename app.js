bf32e5c77433adee5ade84de6df681ca9a833aa0
olivialms <c.olivia@escola.pr.gov.br>
2025-09-24 23:22:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+fuction criaCartao (categoria, pergunta, resposta);
+console.log(categoria, pergunta, resposta)
+}
 
+let container = document.getElementById('container')
+let cartao = document.createElement('article')
+cartao.className = 'cartao'
+
+cartao.innerHTML = `
+                <div class="cartao__conteudo">
+                    <h3>Programação</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>O que é CSS?</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>O CSS é uma linguagem de estilização</p>
+                    </div>
+                </div>
